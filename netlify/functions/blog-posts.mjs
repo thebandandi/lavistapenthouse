0e01a60a20bebcd0d70504dd8728be68096c58aa
thebandandi <Bjsanchez@comcast.net>
2026-04-18 13:19:30 -0600

    Update blog-posts.mjs

--- netlify/functions/blog-posts.mjs
+++ netlify/functions/blog-posts.mjs
@@ -2,7 +2,7 @@ import { getStore } from "@netlify/blobs";
 
 export default async (req, context) => {
   // STEP 1: Try 'posts'. If still empty after deploy, change this to 'site'
-  const STORE_NAME = "blog"; 
+  const STORE_NAME = "content"; 
   
   try {
     const store = getStore(STORE_NAME);
